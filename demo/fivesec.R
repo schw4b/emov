a1ab70075dd587dcb61d1330306a5cb63707d3c5
Simon Schwab <schwab@puk.unibe.ch>
2015-06-07 22:59:58 +0200

    Update fivesec.R

--- demo/fivesec.R
+++ demo/fivesec.R
@@ -7,7 +7,7 @@ fivesec$y = filter(fivesec$y, rep(1/3, 3))
 
 # data is in seconds, 200Hz
 # dispersion, e.g. 2 cm at a viewing distance of 80 cm is ~1.4 deg
-# minimal fixation duration 0.1 s -> 40 fivesecs
+# minimal fixation duration 0.1 s is 20 samples
 fixations = emov.idt(fivesec$time, fivesec$x, fivesec$y, 2, 20)
 
 plot(fivesec$x, fivesec$y, xlim=c(-20,30), ylim=c(-12,-6), type="l", 
